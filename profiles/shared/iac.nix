86e7ecf2623eb8df7168a084805c07efbe3bfcad
WillGle <cuongsayyay@gmail.com>
2026-05-23 15:12:17 +0700

    chore: drop old shared role paths

--- profiles/shared/iac.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }:
-
-{
-  environment.systemPackages = with pkgs; [
-    ansible
-    terraform # Or 'opentofu' if you prefer the open-source fork
-    terraform-ls # Language server for VSCode support
-  ];
-}
