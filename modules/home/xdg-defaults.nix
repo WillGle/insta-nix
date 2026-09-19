57250d3f1f4d91b8935ef1eea772bb158812bbdf
WillGle <cuongsayyay@gmail.com>
2026-09-19 17:39:16 +0700

    fix: correct Signal desktop MIME target

--- modules/home/xdg-defaults.nix
+++ modules/home/xdg-defaults.nix
@@ -29,8 +29,8 @@
         "x-scheme-handler/https" = "brave-browser.desktop";
         "x-scheme-handler/about" = "brave-browser.desktop";
         "x-scheme-handler/unknown" = "brave-browser.desktop";
-        "x-scheme-handler/sgnl" = "signal.desktop";
-        "x-scheme-handler/signalcaptcha" = "signal.desktop";
+        "x-scheme-handler/sgnl" = "org.signal.Signal.desktop";
+        "x-scheme-handler/signalcaptcha" = "org.signal.Signal.desktop";
         "text/plain" = "code.desktop";
         "application/pdf" = "draw.desktop";
 
